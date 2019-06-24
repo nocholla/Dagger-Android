acc5964e634e34c9d40f72b8aa2d9738114d9df8
NICHOLAS OCHOLLA <nicholasocholla@NICHOLASs-MacBook-Air-2.local>
2019-06-24 12:44:31 -0700

    Added MyApplication Class after adding dagger package files

--- app/src/main/java/com/nocholla/dagger/recyclerview/MyApplication.java
+++ app/src/main/java/com/nocholla/dagger/recyclerview/MyApplication.java
@@ -9,22 +9,22 @@ import com.nocholla.dagger.recyclerview.dagger.module.ContextModule;
 
 public class MyApplication extends Application {
 
-//    ApplicationComponent applicationComponent;
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//
-//        applicationComponent = DaggerApplicationComponent.builder().contextModule(new ContextModule(this)).build();
-//        applicationComponent.injectApplication(this);
-//
-//    }
-//
-//    public static MyApplication get(Activity activity){
-//        return (MyApplication) activity.getApplication();
-//    }
-//
-//    public ApplicationComponent getApplicationComponent() {
-//        return applicationComponent;
-//    }
+    ApplicationComponent applicationComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        applicationComponent = DaggerApplicationComponent.builder().contextModule(new ContextModule(this)).build();
+        applicationComponent.injectApplication(this);
+
+    }
+
+    public static MyApplication get(Activity activity){
+        return (MyApplication) activity.getApplication();
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+        return applicationComponent;
+    }
 }
